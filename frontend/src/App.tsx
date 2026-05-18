bb611dbd5dd05e7f9cd18fe59d0b41c4004f1700
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-05-18 14:04:34 +0530

    structure setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red font-bold">Welcome to Vite + React</h1>
+      <Button>Click_me</Button>
+    </>
+  );
+}
+
+export default App;
